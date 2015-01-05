6c159ab3a1bdc0016bd66684f955c148d54bf924
AlessandroBregoli <alessandroxciv@gmail.com>
2015-01-05 11:37:47 +0100

    Creazione classe di interfaccia Albero

---
+++ Albero.java
@@ -0,0 +1,53 @@
+public class  Albero{
+	priate Nodo albero;
+	private boolean outOfTree;
+	public Albero(){
+			albero = null;
+			this.outOfTree = false;
+	}
+	public void addNodo(String nome){
+		if(this.outOfTree)
+			return;
+		Nodo n = new Nodo();
+		n.nome = nome;
+		if(this.albero == null){
+			this.albero = n;
+			this.albero.parent = null;
+		}
+		else{
+			n.parent = this.albero;
+			this.albero.figli.add(n);
+			this.albero = n;
+		}
+	}
+	public void addPCDATA(String s){
+		if(this.outOfTree)
+			return;
+		if(albero == null){
+			throw new Exception("Hai sbagliato qualcosa di grosso");			
+		}
+		else{
+			this.albero.figli.add(s);
+		}
+	}
+	
+	public void addAttributes(HashMap<String,String> s){
+		if(this.outOfTree)
+			return;
+		this.albero.attributi = s;
+	}
+	
+	public void up(){
+		if(this.albero.parent != null){
+			this.albero = this.albero.parent;
+		}
+		else
+			this.outOfTree = true;
+	}
+	
+	public String toJSON(){
+		if(this.albero.parent != null)
+			throw new Exception("XML non completo");
+		return this.albero.toJSON(0);
+	}
+}
