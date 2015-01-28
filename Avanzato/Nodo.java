2bb05a4ca626bcbbbd6b5691e88fcec4299356c2
AlessandroBregoli <alessandroxciv@gmail.com>
2015-01-28 13:16:37 +0100

    Aggiunto toXML

--- Avanzato/Nodo.java
+++ Avanzato/Nodo.java
@@ -8,45 +8,26 @@ public class Nodo{
 		this.attributi = new HashMap<String, String>();
 		this.figli = new ArrayList<Object>();
 	}
-	public String toJSON(int indentazione){
-		String JSON = "";
-		String tabg = Nodo.retTab(indentazione);
-		String tab = Nodo.retTab(indentazione + 1);
-		String tab2 = Nodo.retTab(indentazione + 2);
-		JSON = tabg + "{\n";
-		JSON = JSON + tab + "\"tag\": \"" + this.nome + "\",\n";
-		for(String key : this.attributi.keySet()){
-			JSON = JSON + tab + "\"@" + key + "\": " + sistemaAcapi(this.attributi.get(key), tab) + ",\n";
-		}
-		if(this.figli.size() != 0){
-			JSON = JSON + tab + "\"content\": [\n";
-			boolean primo = true;
-			for(Object n: this.figli){
-				boolean isNode = false;
-				String stringa = "";
-				if(!primo){
-					JSON = JSON +",\n";
-				}
-				primo = false;
-				try{
-					stringa = (String)n;
-				}catch(Exception e){
-					isNode = true;
-				}
-				if(isNode){
-					JSON = JSON + ((Nodo)n).toJSON(indentazione + 2);
-				}
-				else{
-					JSON = JSON + tab2 + "\"" + sistemaAcapi(stringa, tab2)+"\"";
-				}
-			}
-			JSON = JSON + "\n" + tab + "]\n";
-		}
-		JSON = JSON + tabg + "}";
+	public String toXML(int indentazione){
+		String XML = "";
 		if(indentazione == 0){
-			JSON = JSON + "\n";
+			XML = "<?xml version=\"1.0\" encoding=\"utf-8\"?> \n <!DOCTYPE book SYSTEM \"book.dtd\">\n";
 		}
-		return JSON;
+		XML = XML + Nodo.retTab(indentazione) + "<" + this.nome;
+		for(String s: this.attributi.keySet()){
+			XML = XML + " " + s + "=" + this.attributi.get(s);
+		}
+		XML = XML + ">\n";
+		for(Object n: this.figli){
+			try{
+				String s = (String) n;
+				XML = XML + Nodo.retTab(indentazione + 1) + Nodo.sistemaAcapi(s, Nodo.retTab(indentazione + 1)) + "\n";
+			}catch(Exception e){
+				XML = XML + ((Nodo)n).toXML(indentazione + 1)+ "\n";
+			}
+		}
+		XML = XML + Nodo.retTab(indentazione) + "</" + this.nome + ">";
+		return XML;		
 	}
 	private static String retTab(int n){
 		String ret = "";
@@ -55,20 +36,19 @@ public class Nodo{
 		return ret;
 	}
 	
+	public static String sistemaAcapi(String s, String tabulazz){
+		return s.replaceAll("\t","").replaceAll("\n", "\\\\\n"+tabulazz).replaceAll("\r","");
+	}
+	
 	public static void main(String args[]){
 		Nodo n = new Nodo();
 		Nodo g = new Nodo();
 		n.nome = "book";
 		n.attributi.put("id", "ciao");
-		n.parent = null;
 		n.figli.add("Ciao questa è una prova");
 		n.figli.add(g);
 		g.nome = "bho";
 		g.figli.add("roba sotto");
-		g.parent = n;
-		System.out.print(n.toJSON(0));
-	}
-	public static String sistemaAcapi(String s, String tabulazz){
-		return s.replaceAll("\t","").replaceAll("\n", "\\\\\n"+tabulazz).replaceAll("\r","");
+		System.out.print(n.toXML(0));
 	}
 }
